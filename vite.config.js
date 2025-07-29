b70aaf4bb2a61a46eb1be726850212b5b0432b09
anil-yeddula <vikasyeddula@gmail.com>
2025-07-29 16:17:43 +0530

    ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      '@mui/styled-engine': '@mui/styled-engine-sc'
+    }
+  }
 })
+
